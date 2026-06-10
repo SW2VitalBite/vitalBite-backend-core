49bd67f3d937f0176767160173877ade350a3301
Alvaro <alvaroguzman348@gmail.com>
2026-06-09 20:46:26 -0400

    Enable Cors

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,11 @@ async function bootstrap() {
     }),
   );
 
-  app.enableCors();
+  // app.enableCors();
+  app.enableCors({
+    origin: 'http://18.188.25.170:8080', // En producción, cámbialo por el dominio de tu frontend
+    credentials: true,
+  });
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('port', 3000);
